0b3abd6c3afc97ee904fa19390e0f5d3efdb0aa1
Jakob M <jakobmajid00@gmail.com>
2022-03-02 10:39:58 +0100

    Tilfoejet UTC/ timezone

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 const url = "http://worldtimeapi.org/api/timezone/";
 const time = document.getElementById("time");
+const UTC = document.getElementById("UTC");
 
 const dropdown = document.getElementById("dropdown");
 dropdown.addEventListener("change", () => getTime(url + dropdown.value));
+
+function worldApiFetch(url) {
+  return fetch(url).then(res => res.json());
+}
 
 async function getTime(url) {
   let json = await worldApiFetch(url);
   let dateTime = String(json.datetime);
   time.textContent = dateTime.slice(11, 19);
+  UTC.textContent = String(json.utc_offset);
 }
 
-function worldApiFetch(url) {
-  return fetch(url).then(res => res.json());
-}
+
